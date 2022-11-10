cc3ea6fa05b8dc00a282f0c094a7054a1b623bdd
abhiraj0603 <97404865+abhiraj0603@users.noreply.github.com>
2022-11-11 01:47:31 +0530

    Delete src directory

--- src/main/java/com/musixon/musicplaylist/exception/MusicNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.musixon.musicplaylist.exception;
-
-public class MusicNotFoundException extends RuntimeException {
-    public MusicNotFoundException(String message) {
-        super(message);
-    }
-}
