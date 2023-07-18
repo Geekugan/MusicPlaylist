66740a573e28ccd38227a48d8b0b49eb2e37c775
Geekugan <117636349+Geekugan@users.noreply.github.com>
2023-07-18 23:50:52 +0530

    Update Music.java

--- src/main/java/com/musixon/musicplaylist/model/Music.java
+++ src/main/java/com/musixon/musicplaylist/model/Music.java
@@ -1,22 +1,40 @@
 package com.musixon.musicplaylist.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
+
 @Entity
 public class Music implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false, updatable = false)
     private Long id;
+
+    @NotBlank
     private String song;
+
+    @NotBlank
     private String singer;
+
+    @NotBlank
     private String genre;
+
     private String likes;
+
+    @NotBlank
     private String imageUrl;
+
+    @NotBlank
     @Column(nullable = false, updatable = false)
+    @Pattern(regexp = "^[A-Za-z0-9]+$")
     private String musicCode;
 
-    public Music() {}
+    public Music() {
+    }
+
     public Music(Long id, String song, String singer, String genre, String likes, String imageUrl, String musicCode) {
         this.id = id;
         this.song = song;
@@ -79,9 +97,9 @@ public class Music implements Serializable {
         return musicCode;
     }
 
-
-
     public void setMusicCode(String musicCode) {
         this.musicCode = musicCode;
     }
+
+    
 }
